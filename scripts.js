5d185483cb59fafe66bdf82ffc60eb13a90a14d7
GLMike <shane2michael@gmail.com>
2020-11-01 16:10:56 -0500

    Edited scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,22 @@
-var x = document.getElementById("email").pattern
-console.log(x)
+window.addEventListener('load', function(){
+
+    let button = document.querySelector('.newsletter button');
+    let email = document.getElementById('email');
+    let msg = document.querySelector('.message');
+
+    button.addEventListener('click', function(){
+        event.preventDefault();
+        let val = email.value;
+        
+        if(val === ''){
+            msg.textContent = ("Enter Valid Email Address");
+        }
+        
+        else{
+            msg.innerHTML = `Thank you! Your email address ${val} has been added.`;
+        }
+        
+        email.value = '';
+    });
+});
+
